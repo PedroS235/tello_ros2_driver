bf28905a996c0bbb7fecbce63db683f29e69385c
Pedro Soares <pmbs.123@gmail.com>
2023-03-24 21:41:49 +0100

    [feat] Inlude the params file when launching the node

--- tello_driver/launch/tello_driver.launch.py
+++ tello_driver/launch/tello_driver.launch.py
@@ -1,11 +1,17 @@
+import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python import get_package_share_directory
 
 
 def generate_launch_description():
+    pkg_dir = get_package_share_directory("tello_driver")
+    param_file = os.path.join(pkg_dir, "config", "params.yaml")
+
     tello_driver_cmd = Node(
         package="tello_driver",
         executable="tello_driver",
+        parameters=[param_file],
         output="screen",
     )
 
